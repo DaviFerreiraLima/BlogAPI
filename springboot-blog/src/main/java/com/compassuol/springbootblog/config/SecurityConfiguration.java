2f820a857c2f30cfe259091698183c40b2550004
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-06-30 15:53:51 -0300

    Feat: Adding Spring security to the api, with user,roles , Custom user Details service and exception for unauthorized auth

---
+++ springboot-blog/src/main/java/com/compassuol/springbootblog/config/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package com.compassuol.springbootblog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
+
+@Configuration
+public class SecurityConfiguration {
+
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    SecurityConfiguration(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) ->
+                        authorize.requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
+                                .anyRequest().authenticated()
+
+                ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
